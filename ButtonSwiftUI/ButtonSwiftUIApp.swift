f64686850d679e8c44dc0350b788be6bcaaa9f7c
hafizhMo <hafizh.hizb@gmail.com>
2022-05-15 22:43:24 +0700

    Initial Commit

---
+++ ButtonSwiftUI/ButtonSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ButtonSwiftUIApp.swift
+//  ButtonSwiftUI
+//
+//  Created by Hafizh Mo on 15/05/22.
+//
+
+import SwiftUI
+
+@main
+struct ButtonSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ButtonSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ButtonSwiftUI
+//
+//  Created by Hafizh Mo on 15/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
